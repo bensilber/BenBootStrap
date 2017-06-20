6580689de6aa5c8e84624e2b9eb88051694b6482
bensilber <bsilber@gmail.com>
2017-06-20 11:21:46 -0400

    blah

--- brewManifest.sh
+++ brewManifest.sh
@@ -3,9 +3,11 @@
 #Check if Homebrew is installed
 
 which -s brew
-if [[ $? != 0 ]] ; then
+if [[ $? != 0 ]] | echo "Brew not detected; Installing now." ; then 
     # Install Homebrew
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
     brew update
 fi
+
+#Add various Casks
