10deb78d8a764f46e2f099269448bf23abbcd47e
bensilber <bsilber@gmail.com>
2017-06-20 11:16:40 -0400

    Initial Commit!

---
+++ brewManifest.sh
@@ -0,0 +1,11 @@
+!# /bin/bash
+
+#Check if Homebrew is installed
+
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
